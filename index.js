7b0e3adcfdca1cf0e38f2a3ece87a642a9127ab9
Nick Zelei <nickzelei@gmail.com>
2016-10-16 16:50:03 -0500

    Working proto

--- index.js
+++ index.js
@@ -1,35 +1,73 @@
 const FeedParser = require("feedparser");
 const request = require("request");
+const Promise = require("bluebird");
+const flatten = require("lodash").flatten;
+const nodemailer = require("nodemailer");
 
-const lainacclaim = "https://kansascity.craigslist.org/search/sss?format=rss&query=lane%20acclaim";
+const cfg = require("dotenv").config();
+const readUrls = require("./urls.json");
 
-const items = [];
+const EMAIL_OPTIONS = {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: cfg.EMAIL,
+            pass: cfg.APP_PASS
+        }
+};
 
-const req = request(lainacclaim);
+const urls = Object.keys(readUrls).filter((url) => readUrls[url]);
 
-const feeder = new FeedParser();
+Promise.map(urls, (url) => {
+    return new Promise((resolve, reject) => {
+        const items = [];
 
-req.on("error", console.error);
+        const req = request(url);
+        const feeder = new FeedParser();
 
-req.on('response', function (res) {
- 
-  if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+        req.on("error", reject);
 
-  res.pipe(feeder);
-});
+        req.on('response', function (res) {
+            if (res.statusCode != 200) return reject(new Error("Something went wrong."));
 
-feeder.on("error", console.error);
-feeder.on("end", () => parseItems());
+            res.pipe(feeder);
+        });
 
-feeder.on("readable", function () {
-   let item;
+        feeder.on("error", reject);
 
-    while (item = this.read()) {
-        items.push(item);
-    }
-});
+        feeder.on("readable", function () {
+            let item;
 
-function parseItems() {
-    items.map((item) => console.log(item.title, item.link));
-    //todo send email
+            while (item = this.read()) {
+                items.push(item);
+            }
+        });
+
+        feeder.on("end", () => {
+            return resolve(items.map((item) => {
+                return {
+                    title: item.title,
+                    link: item.link
+                };
+            }));    
+        });
+    });
+}).then(flatten).then(email).catch(console.error);
+
+function email(items) {
+    const transporter = nodemailer.createTransport(EMAIL_OPTIONS);
+
+    const mail = {
+        from: cfg.EMAIL,
+        to: cfg.EMAIL,
+        subject: "New Craigslist Finds",
+        html: items.map((item) => `<a href=${item.link}>${item.title}</a>`).join("<br /><br />")
+    };
+
+    transporter.sendMail(mail, (err, info) => {
+        if (err) { return console.error(err); }
+
+        console.log("Mail Sent: " + info.response);
+    });
 }
